c499a6282f4422c598e31861e68ab5d004ebb53d
LucasJSilva <LucasJSilva_0xF0@protonmail.com>
2024-09-29 15:31:42 -0300

    Add click_sleep function

--- bot/lib/fiscal.py
+++ bot/lib/fiscal.py
@@ -2,6 +2,11 @@ import pyautogui
 import time
 
 
+def click_sleep(x, y, sleep=0.5):
+    pyautogui.click(x, y)
+    time.sleep(sleep)
+
+
 def click_sleep_write(x, y, s):
     pyautogui.click(x, y)
     time.sleep(0.2)
@@ -9,25 +14,25 @@ def click_sleep_write(x, y, s):
     pyautogui.write(s, 0.1)
     time.sleep(0.2)
 
+    pyautogui.press('enter')
+
 
 def standart_sp():
-    pyautogui.click(35, 255)
-    time.sleep(0.5)
-
+    click_sleep(35, 255)
     click_sleep_write(490, 320, "49")
     click_sleep_write(490, 350, "49")
     click_sleep_write(260, 375, "1")
 
 
 def standart_ms():
-    pyautogui.click(110, 255)
-    time.sleep(0.5)
-
+    click_sleep(110, 255)
     click_sleep_write(490, 320, "49")
     click_sleep_write(490, 350, "49")
     click_sleep_write(260, 375, "104")
 
 
 def fiscal():
+    click_sleep(150, 130)
+
     standart_sp()
     standart_ms()
